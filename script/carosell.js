ebb3e2653a95577050639bee1ad3faf789b76f31
Josh <row.josh02@gmail.com>
2024-06-04 14:34:12 +0100

    First Structure

---
+++ script/carosell.js
@@ -0,0 +1,13 @@
+$('.home-hero').Carousel({
+  autoplay: true,
+  autoplayTimeout: 7500,
+  autoplayHoverPause: true,
+  center: true,
+  loop: true,
+  nav: true,
+  navText: [
+    "<span class='fa fa-angle-left'></span>",
+    "<span class='fa fa-angle-right'></span>",
+  ],
+  items: 1,
+});
